eab1996a0a0f7a4e484cfde3b0184dfa3a5a21e9
Manoranjan D Salunke <manoranjan710@gmail.com>
2026-01-22 13:43:15 +0530

    FIx: Home link commented out and breadcrumb bottom margin fixed

--- src/components/common/PageBreadCrumb.tsx
+++ src/components/common/PageBreadCrumb.tsx
@@ -7,7 +7,7 @@ interface BreadcrumbProps {
 
 const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
   return (
-    <div className="flex flex-wrap items-center justify-between gap-3 mb-6">
+    <div className="flex flex-wrap items-center justify-between gap-3">
       <h2
         className="text-xl font-semibold text-gray-800 dark:text-white/90"
         x-text="pageName"
@@ -17,7 +17,7 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
       <nav>
         <ol className="flex items-center gap-1.5">
           <li>
-            <Link
+            {/* <Link
               className="inline-flex items-center gap-1.5 text-sm text-gray-500 dark:text-gray-400"
               href="/"
             >
@@ -38,7 +38,7 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
                   strokeLinejoin="round"
                 />
               </svg>
-            </Link>
+            </Link> */}
           </li>
           <li className="text-sm text-gray-800 dark:text-white/90">
             {pageTitle}
